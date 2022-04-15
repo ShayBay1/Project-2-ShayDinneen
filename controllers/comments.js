ea1343984037f1c47cc0793219ac6128c58e3a17
shaybay <pacificconnect310@gmail.com>
2022-04-15 07:56:52 -0700

    updated CSS

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,18 +3,12 @@ const Slam = require('../models/Slam')
 function addcomment (req, res) {
 
     Slam.findById(req.params.id, function(err, comment) {
-        // Update req.body to contain user info
-        console.log(req.body, '<------pre')
-        console.log(req.user, '<------user')
         req.body.commentUserAvatar = req.user.avatar;
         req.body.user = req.user._id;
         req.body.username = req.user.name;
-        // Add the comment
         comment.comments.push(req.body);
-        console.log(comment, '<----------helloooo')
         comment.save(function(err) {
-            console.log(req.body, '<------post')
-          res.redirect(`/slams/${comment._id}`);
+        res.redirect(`/slams/${comment._id}`);
         });
     });
 }
--- controllers/slams.js
+++ controllers/slams.js
@@ -1,28 +1,17 @@
 const Slam = require('../models/Slam');
 const User = require('../models/User');
 
-
-
-// future function for adding comment to slam post
-// req.body.userId = req.user_id;
-    // req.body.userName = req.user.name;
-    // slam.comments.push(req.body);
-    // comment.save(function(err){
-
 function newSlam(req, res){
     res.render('slams/new');
 }
-
 function create(req, res){
-    console.log(req.body, '<-----pre')
     req.body.userAvatar = req.user.avatar;
     req.body.userName = req.user.name;
     req.body.comments = [];
-    console.log(req.body, '<-----post')
     const slamPost = new Slam(req.body);
-if (req.body.link) 
-slamPost.save(function(err){
-    if (err) return res.redirect('/');
+        if (req.body.link) 
+    slamPost.save(function(err){
+        if (err) return res.redirect('/');
     res.redirect('/');   
 });
 }
@@ -35,24 +24,19 @@ function deleteSlam (req, res){
 }
 function index(req, res){
     Slam.find({}, function(err, slams){
-        console.log(slams, '<----this is what slams looks like')
-        res.render('index', {
-            slams
-        })
+        res.render('index', {slams})
     });
 }
 function show (req, res){
     Slam.findById(req.params.id, function(err, slams){
-        console.log(slams, '<----------helloooo')
-        res.render('slams/show', {slams, title: 'Slam Details'});
-        });
-      }; 
-
+    res.render('slams/show', {slams, title: 'Slam Details'});
+    });
+}; 
 
 module.exports = {
     new: newSlam,
     create,
     index,
     show,
-    deleteSlam,
+    deleteSlam,  
 }
